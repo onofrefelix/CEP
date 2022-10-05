05889a8993d040e19ea55e3d34834826aeb96e57
onofr <onofre.felix@domrock.ai>
2022-10-05 18:18:23 -0300

    Test of commit

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"CEP/files"
 	"CEP/tools"
-	"fmt"
+	"github.com/go-gota/gota/dataframe"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -18,6 +21,29 @@ func main() {
 		print(err)
 	}
 
-	fmt.Println(tools.Filter(listFile, ".TXT")[0:])
+	listf := tools.Filter(listFile, ".TXT")
+
+	var dataFrameList []dataframe.DataFrame
+	var df dataframe.DataFrame
+
+	for _, file := range listf {
+		df, err = ReadData(file)
+		if err != nil {
+			log.Fatalf("data can't be reading")
+		}
+		dataFrameList = append(dataFrameList, df)
+	}
+
+}
+
+func ReadData(filename string) (dataframe.DataFrame, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return dataframe.DataFrame{}, err
+	}
+
+	ioContent := strings.NewReader(string(content))
+
+	return dataframe.ReadCSV(ioContent, dataframe.WithDelimiter('@')), nil
 
 }
