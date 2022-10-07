60bfeba92f87b6fb0d04dd248ca67c95fb4d0195
domrock <onofre.felix@domrock.ai>
2022-10-06 21:08:29 -0300

    Contitnuaçao do projeto de CEP

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"CEP/files"
 	"CEP/tools"
+	"fmt"
 	"github.com/go-gota/gota/dataframe"
+	"golang.org/x/text/encoding/charmap"
 	"log"
 	"os"
-	"strings"
 )
+
+type CepConfig struct {
+	Logradouro  string
+	Bairro      string
+	FaixaBairro string
+	Cpcd        string
+}
 
 func main() {
 	var orig, dest string
@@ -15,35 +23,73 @@ func main() {
 	orig = "./data/atualiza_base_cep_out_2022.zip"
 	dest = "./data/unzipfiles/"
 
+	cepData := CepConfig{
+		Logradouro:  "LOG_LOGRADOURO",
+		Bairro:      "LOG_BAIRRO",
+		FaixaBairro: "LOG_BAIRRO",
+		Cpcd:        "LOG_CPC",
+	}
+
 	files.Unzip(orig, dest)
 	listFile, err := files.ScanDir(dest)
 	if err != nil {
 		print(err)
 	}
 
-	listf := tools.Filter(listFile, ".TXT")
+	listf := tools.Filter(listFile, cepData.Logradouro)
 
-	var dataFrameList []dataframe.DataFrame
+	//var dataFrameList []dataframe.DataFrame
 	var df dataframe.DataFrame
 
-	for _, file := range listf {
-		df, err = ReadData(file)
-		if err != nil {
-			log.Fatalf("data can't be reading")
-		}
-		dataFrameList = append(dataFrameList, df)
+	df, err = ReadData(listf)
+	if err != nil {
+		log.Fatalf("data can't be reading")
 	}
+
+	fmt.Println(df) //df.Select([]int{1, 2, 3, 7))
+	fmt.Println(df.Describe())
 
 }
 
-func ReadData(filename string) (dataframe.DataFrame, error) {
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		return dataframe.DataFrame{}, err
+func ReadData(listOfFilename []string) (dataframe.DataFrame, error) {
+
+	//var temp string
+	//var content []byte
+	//var err error
+	//
+	//for _, filename := range listOfFilename {
+	//	content, err = os.ReadFile(filename)
+	//	if err != nil {
+	//		return dataframe.DataFrame{}, err
+	//	}
+	//
+	//	temp += string(content)
+	//}
+	//ioContent := strings.NewReader(string(content))
+	//
+	//return dataframe.ReadCSV(ioContent, dataframe.WithDelimiter('@'),
+	//	dataframe.WithLazyQuotes(true),
+	//	dataframe.HasHeader(false)), nil
+
+	//var temp string
+	//var content []byte
+
+	var df, dfConcat dataframe.DataFrame
+
+	for _, filename := range listOfFilename {
+		content, err := os.Open(filename)
+		if err != nil {
+			return dataframe.DataFrame{}, err
+		}
+
+		//ioContent := strings.NewReader(string(content))
+		ioContent := charmap.ISO8859_1.NewDecoder().Reader(content)
+		df = dataframe.ReadCSV(ioContent, dataframe.WithDelimiter('@'),
+			dataframe.WithLazyQuotes(true),
+			dataframe.HasHeader(false))
+		dfConcat = dfConcat.Concat(df)
+
 	}
 
-	ioContent := strings.NewReader(string(content))
-
-	return dataframe.ReadCSV(ioContent, dataframe.WithDelimiter('@')), nil
-
+	return dfConcat, nil
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module CEP
 
 go 1.19
 
-require github.com/go-gota/gota v0.12.0
+require (
+	github.com/go-gota/gota v0.12.0
+	golang.org/x/text v0.3.6
+)
 
 require (
 	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
